14a144855134de6cd5a61ec1d647c8756b90fde3
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-10-31 16:38:27 +0900

    サンプルAPIを追加

--- resources/js/components/ComponentA.vue
+++ resources/js/components/ComponentA.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Laravel10 + Vue3 Component Test</h1>
+  <div>
+    <h1>Laravel10 + Vue3 Component Test</h1>
+    <button @click="fetchData">fetch</button>
+    <div v-if="data">
+      <pre>Message: {{ data.message }}</pre>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import axios from "axios";
+import { ref } from "vue";
+
+interface SampleData {
+  message: string;
+}
+
+const data = ref<SampleData | null>(null);
+
+const fetchData = async () => {
+  try {
+    const res = await axios.get("/api/sample");
+    data.value = res.data;
+  } catch (e: any) {
+    console.error("Error: ", e);
+  }
+};
+</script>
