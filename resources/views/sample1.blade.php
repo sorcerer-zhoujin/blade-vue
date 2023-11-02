919109c23908edf9be3afc9442d3c293bdb05df5
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-11-02 13:00:38 +0900

    eslint script

--- resources/views/sample1.blade.php
+++ resources/views/sample1.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Laravel Vite Vue</title>
+  <title>Laravel Vite Vue</title>
 </head>
 
 <body>
-    <div id="app"></div>
-    @vite(['resources/css/app.css', 'resources/js/app.ts'])
+  <div id="app"></div>
+  @vite(["resources/css/app.css", "resources/js/app.ts"])
 </body>
 
 </html>
